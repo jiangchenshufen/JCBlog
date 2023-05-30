8df4f4e99ddb3d03648c3026ad05892903e18c99
姜琛 <896668883@qq.com>
2023-05-30 19:46:36 +0800

    # feat 新增分类信息导出Excel

--- jiangchen-admin/src/main/java/com/jiangchen/controller/CategoryController.java
+++ jiangchen-admin/src/main/java/com/jiangchen/controller/CategoryController.java
@@ -1,14 +1,25 @@
 package com.jiangchen.controller;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.EasyExcelFactory;
+import com.alibaba.fastjson.JSON;
 import com.jiangchen.domain.ResponseResult;
 import com.jiangchen.domain.dto.UpdateCategoryDto;
 import com.jiangchen.domain.dto.addCategoryDto;
+import com.jiangchen.domain.vo.ExcelCategoryVo;
+import com.jiangchen.enums.AppHttpCodeEnum;
 import com.jiangchen.service.CategoryService;
+import com.jiangchen.utils.BeanCopyUtils;
+import com.jiangchen.utils.WebUtils;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/content/category")
@@ -52,5 +63,19 @@ public class CategoryController {
     @DeleteMapping({"{id}"})
     public ResponseResult delCategoryById(@PathVariable("id") Long id){
         return categoryService.delCategoryById(id);
+    }
+
+    @ApiOperation("导出分类列表")
+    @GetMapping("/export")
+    public void exportCategory(HttpServletResponse response){
+        try {
+            WebUtils.setDownLoadHeader("分类.xlsx",response);
+            List<ExcelCategoryVo> excelCategoryVos = BeanCopyUtils.copyBeanList(categoryService.list(), ExcelCategoryVo.class);
+            EasyExcelFactory.write(response.getOutputStream(),ExcelCategoryVo.class).autoCloseStream(Boolean.FALSE)
+                    .sheet("分类导出").doWrite(excelCategoryVos);
+        } catch (Exception e) {
+            ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR);
+            WebUtils.renderString(response, JSON.toJSONString(result));
+        }
     }
 }
---
+++ jiangchen-framework/src/main/java/com/jiangchen/domain/vo/ExcelCategoryVo.java
@@ -0,0 +1,19 @@
+package com.jiangchen.domain.vo;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExcelCategoryVo {
+    @ExcelProperty("分类名")
+    private String name;
+    @ExcelProperty("描述")
+    private String description;
+    @ExcelProperty("状态")
+    private String status;
+
+}
